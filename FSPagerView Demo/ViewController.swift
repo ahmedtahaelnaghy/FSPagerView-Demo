908f0b99a3c0e65073f9d65ffbcb001b3bdc5fc5
Ahmed Taha <ahmed.taha8399@gmail.com>
2022-09-15 16:00:29 +0200

    Implement the FSPagerView pod in the demo with nine designs in view

--- FSPagerView Demo/ViewController.swift
+++ FSPagerView Demo/ViewController.swift
@@ -7,13 +7,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import FSPagerView
 
+class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate {
+    
+    fileprivate let imageNames = ["1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12"]
+    fileprivate let transformerNames = ["cross fading", "zoom out", "depth", "linear", "overlap", "ferris wheel", "inverted ferris wheel", "coverflow", "cubic"]
+    fileprivate let transformerTypes: [FSPagerViewTransformerType] = [.crossFading,
+                                                                      .zoomOut,
+                                                                      .depth,
+                                                                      .linear,
+                                                                      .overlap,
+                                                                      .ferrisWheel,
+                                                                      .invertedFerrisWheel,
+                                                                      .coverFlow,
+                                                                      .cubic]
+    fileprivate var typeIndex = 0 {
+        didSet {
+            let type = self.transformerTypes[typeIndex]
+            self.pagerView.transformer = FSPagerViewTransformer(type:type)
+            switch type {
+            case .crossFading, .zoomOut, .depth:
+                self.pagerView.itemSize = FSPagerView.automaticSize
+                self.pagerView.decelerationDistance = 1
+            case .linear, .overlap:
+                let transform = CGAffineTransform(scaleX: 0.6, y: 0.75)
+                self.pagerView.itemSize = self.pagerView.frame.size.applying(transform)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .ferrisWheel, .invertedFerrisWheel:
+                self.pagerView.itemSize = CGSize(width: 180, height: 140)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .coverFlow:
+                self.pagerView.itemSize = CGSize(width: 220, height: 170)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .cubic:
+                let transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                self.pagerView.itemSize = self.pagerView.frame.size.applying(transform)
+                self.pagerView.decelerationDistance = 1
+            }
+        }
+    }
+    
+    @IBOutlet weak var pagerView: FSPagerView! {
+        didSet {
+            self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+            self.typeIndex = 7
+        }
+    }
+    
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let index = self.typeIndex
+        self.typeIndex = index // Manually trigger didSet
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pagerView.delegate = self
+        pagerView.dataSource = self
     }
-
-
+    
+    // MARK:- FSPagerViewDataSource
+    
+    public func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return imageNames.count
+    }
+    
+    public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.imageView?.image = UIImage(named: self.imageNames[index])
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.clipsToBounds = true
+        return cell
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
+    }
+    
 }
 
